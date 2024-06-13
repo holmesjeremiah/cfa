adc590494158f148cacc48b2a08b1a2419507132
frogman <contact@jeremiah.business>
2024-06-12 20:55:20 -0400

    updated the header font size to be responsive

--- src/HomePage/Header.js
+++ src/HomePage/Header.js
@@ -1,18 +1,31 @@
+import React, { useState, useEffect } from 'react';
 import { IoMdInformationCircleOutline } from "react-icons/io";
 import headerPhoto from './headerPhoto.jpg'
 
 const Header = () => {
+    const [deviceIsMobile, setDeviceIsMobile] = useState(false);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setDeviceIsMobile(window.innerWidth <= 768);
+        };
+
+        handleResize();
+        window.addEventListener('resize', handleResize);
+
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
     return (
         <div className="container" style={{ display: 'flex', justifyContent: 'center' }} >
             <div className="row">
                 <div className="col-sm-12 col-md-6" >
-                    <div style={{ fontSize: '80px', lineHeight: '1', paddingBottom: '25px' }}>
-                        Unlock the Potential of Your Business  <span style={{ color: '#41628B' }}>Today!</span>
+                    <div style={{ fontSize: deviceIsMobile ? '50px' : '80px', lineHeight: '1', paddingBottom: '25px' }}>
+                        Unlock the Potential of Your Business  <span style={{ color: '#41628B', }}>Today!</span>
                     </div>
                     <div>
-                        We believe in providing you with a seamless experience, allowing you to focus on what matters most—growing your business. Start your journey with us and discover the difference!
+                        At Commercial Funders of America, we believe in providing you with a seamless experience, allowing you to focus on what matters most—growing your business. Start your journey with us and discover the difference!
                     </div>
-                    <div style={{ display: 'flex', alignContent: 'end' }}>
+                    <div style={{ display: 'flex', alignContent: 'end', flexWrap: 'wrap', }}>
                         <div style={{ borderLeft: '2px solid #FF5757', margin: '25px', paddingLeft: '10px' }}>
                             <span style={{ fontSize: '25px' }}>No</span><br />
                             Application Fee
@@ -21,7 +34,10 @@ const Header = () => {
                             <span style={{ fontSize: '25px' }}>No</span><br />
                             Additional Upfront Fees
                         </div>
-
+                        <div style={{ borderLeft: '2px solid #FF5757', margin: '25px', paddingLeft: '10px' }}>
+                            <span style={{ fontSize: '25px' }}>No</span><br />
+                            Hidden Costs
+                        </div>
 
                     </div>
                     <div style={{ display: 'flex', alignItems: 'center' }}>
